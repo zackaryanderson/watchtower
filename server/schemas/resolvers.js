0efcb8f561562e0487e0952816643049cb4dcee8
zanderson1998 <zanderson1998@yahoo.com>
2021-04-20 20:03:48 -0600

    fix bugs to get mongodb server running on backend

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,8 +3,20 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-    },
-    Mutation: {
+        user: async (parent, args, context) => {
+            if (context.user) {
+              const user = await User.findById(context.user._id).populate({
+                path: 'orders.products',
+                populate: 'category'
+              });
+      
+              user.orders.sort((a, b) => b.purchaseDate - a.purchaseDate);
+      
+              return user;
+            }
+      
+            throw new AuthenticationError('Not logged in');
+          },
     }
 };
 
