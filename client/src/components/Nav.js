af8c63201bc1c8ed3ee196a2bd6bc41075694c2d
Gavin Asay <gavin.asay@gmail.com>
2021-04-21 01:25:39 -0600

    create header/nav componenets

---
+++ client/src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Nav() {
+	return (
+		<nav>
+			<ul>
+				<li>Home</li>
+				<li>My Dashboard</li>
+				<li>My Community</li>
+				<li>Guides</li>
+			</ul>
+		</nav>
+	);
+}
+
+export default Nav;
