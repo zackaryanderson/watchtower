e460a7d1ed51f6948cc48ab7e7034a89ca096ed4
zanderson1998 <zanderson1998@yahoo.com>
2021-04-21 21:59:05 -0600

    add user schema

--- server/models/User.js
+++ server/models/User.js
@@ -3,9 +3,60 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const bcrypt = require('bcrypt');
 
+//import posts, sensors, and switches
+const Post = require('./Post');
+const Sensor = require('./Sensor');
+const Switch = require('./Switch');
+
 const userSchema = new Schema({
 
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 6
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    posts: [Post.schema],
+    sensors: [Sensor.schema],
+    switches: [Switch.schema]
+
 });
+
+// set up pre-save middleware to create password
+userSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+    next();
+});
+
+// compare the incoming password with the hashed password
+userSchema.methods.isCorrectPassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
 
 const User = mongoose.model('User', userSchema);
 
