c9f2d17b8413c734a20814fbd24bc75eca68465a
Bo Daley <70559889+6bodaley6@users.noreply.github.com>
2021-04-26 19:46:50 -0600

    Added packages and proxy so client can talk to server

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,23 @@
+import React from "react"
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+
 import './App.css';
 import Community from './pages/Community'
 import Header from './components/Header';
 import Dashboard from './pages/Dashboard';
 
+const client = new ApolloClient({
+	uri: '/graphql'
+});
+
 function App() {
 	return (
-		<>
+		<ApolloProvider client={client}>
 			<Header />
 			{/* <Dashboard /> */}
 			<Community />
-		</>
+		</ApolloProvider>
 	);
 }
 
